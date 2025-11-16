e06d57c62192e7122e2024a84478a0cf358fc785
Moch Mufiddin <m.mufiddin@gmail.com>
2025-11-16 23:48:10 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,23 @@ import { jwtMiddleware } from "./middlewares/jwt.middleware";
 import { apiTokenGuard } from "./middlewares/token.middleware";
 import { env } from "./env";
 import { openapi } from "@elysiajs/openapi";
+import cors from "@elysiajs/cors";
 
 new Elysia()
-  .use(openapi())
+  // .use(openapi())
+  .use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "https://admin.worldhost.group",
+        "https://admin.hosting.com/",
+        "https://admin.hosting.com/",
+        "https://clients.verpex.com/",
+        "https://itdmyjju7rda.upmind.app",
+        "chrome-extension://*",
+      ],
+    })
+  )
   .get("/", () => "hello")
   .use(authRoute)
 
